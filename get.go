2863e2b34433949b02b75660c9cc5b42c086b431
LukasXavier <lucasxavier@email.arizona.edu>
2023-08-03 18:03:46 -0700

    PC: I'm some what of a `programmer`

--- get.go
+++ get.go
@@ -2,36 +2,99 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"errors"
 	"net/http"
 )
 
-type Team struct {
-    Data []struct {
-        Id string `json:"_id"`;
-        Name string `json:"name"`;
-        Average int `json:"average"`;
-        Games []int `json:"games"`;
-        TeamName string `json:"teamName"`;
-        TeamId string `json:"team"`;
-    } `json:"data"`
+type TeamMember struct {
     Id string;
-    Team string;
+    Name string;
+    Average int;
+    Games []int;
 }
 
-func getIndividualTeam(id string) (*Team, error) {
+type Team struct {
+    Members []TeamMember;
+    Id string;
+    Name string;
+}
+
+func getIndividualTeam(id string) (t *Team, e error) {
     // https://www.leaguepals.com/loadIndividualTeam?id=64775970bba9d14862bcf9ce&noPre=false
-    url := "https://www.leaguepals.com/loadIndividualTeam?id=" + id + "&noPre=false"
-    log.Print(url)
-    if res, err := http.Get(url); err != nil {
-        return nil, err
-    } else {
-        var t Team
-        defer res.Body.Close()
-        if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
-            return nil, err
-        } else {
-            return &t, nil
-        }
+    url := "https://www.leaguepals.com/loadIndividualTeam?noPre=false&id=" + id
+    resp, e := http.Get(url)
+    if e != nil {
+        return nil, e
     }
+    defer resp.Body.Close()
+    var ingest struct {
+        Data []struct {
+            Id string `json:"_id"`;
+            Name string `json:"name"`;
+            // Email string `json:"email"`;
+            // FirstName string `json:"firstName"`;
+            // LastName string `json:"lastName"`;
+            // Dexterity int `json:"dexterity"`;
+            Average int `json:"average"`;
+            // Averages []struct {
+                // League string `json:"league"`;
+                // Team string `json:"team"`;
+                // Id string `json:"_id"`;
+                // IndividualPoints int `json:"individualPoints"`;
+                // TotalPointsCarryOn int `json:"totalPointsCarryOn"`;
+                // GamesCarryOn int `json:"gamesCarryOn"`;
+                // HighSeriesHdcp int `json:"highSeriesHdcp"`;
+                // HighGameHdcp int `json:"highGameHdcp"`;
+                // HighSeries int `json:"highSeries"`;
+                // HighGame int `json:"highGame"`;
+                // FixedAvg *sturct {} `json:"fixedAvg"`;
+                // Average int `json:"average"`;
+            // } `json:"averages"`;
+            // Subs []struct {} `json:"subs"`;
+            // NickNames []string `json:"nickNames"`;
+            // IsJunior bool `json:"isJunior"`;
+            // IsFemale bool `json:"isFemale"`;
+            // Avatar string `json:"avatar"`;
+            Games []int `json:"games"`;
+            // IndPointsWon int `json:"indPointsWon"`;
+            HighGame int `json:"highGame"`;
+            HighGameHdcp int `json:"highGameHdcp"`;
+            HighSeries int `json:"highSeries"`;
+            HighSeriesHdcp int `json:"highSeriesHdcp"`;
+            RealAvg int `json:"realAvg"`;
+            EnteringAvg int `json:"enteringAvg"`;
+            // GamesPlayed int `json:"gamesPlayed"`;
+            TeamName string `json:"teamName"`;
+            Team string `json:"team"`;
+            League string `json:"league"`;
+            LeagueName string `json:"leagueName"`;
+            Center string `json:"center"`;
+            CenterName string `json:"centerName"`;
+            // TotalPointsScored int `json:"totalPointsScored"`;
+        } `json:"data"`;
+    }
+    e = json.NewDecoder(resp.Body).Decode(&ingest)
+    if e != nil {
+        return nil, e
+    }
+    // I need to flatten ingest and only keep the stuff I care about
+    if len(ingest.Data) > 0 {
+        var members []TeamMember
+        for _, data := range ingest.Data {
+            members = append(members, TeamMember{
+                Id: data.Id,
+                Name: data.Name,
+                Average: data.Average,
+                Games: data.Games,
+            })
+        }
+        t = &Team{
+            Name: ingest.Data[0].TeamName,
+            Id: ingest.Data[0].Team,
+            Members: members,
+        }
+    } else {
+        return nil, errors.New("Server returned empty data")
+    }
+    return
 }
